259d770f9b9138d3f4971d22a9ead57a8e52f1f7
Emilka <01197869@pw.edu.pl>
2024-12-20 00:37:29 +0100

    zmieniony main.c

--- src/main.c
+++ src/main.c
@@ -7,30 +7,20 @@ int main(int argc, char ** argv) {
     int res;
     Matrix * A = readFromFile(argv[1]);
     Matrix * b = readFromFile(argv[2]);
-    Matrix * x;
 
     if (A == NULL) return -1;
     if (b == NULL) return -2;
     printToScreen(A);
     printToScreen(b);
 
-    // Wywołanie eliminacji Gaussa, która obejmuje również podstawienie wsteczne
+    // wywołanie eliminacji Gaussa która obejmuje również podstawienie wsteczne
     res = eliminate(A, b);
 
     if (res == 0) {
-        // Utworzenie i wyświetlenie wektora wynikowego x
-        x = createMatrix(b->r, 1);
-        if (x != NULL) {
-            for (int i = 0; i < b->r; i++) {
-                x->data[i][0] = b->data[i][0];  // Zapisz rozwiązanie do x
-            }
-            printToScreen(x);
-            freeMatrix(x);
-        } else {
-            fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
-        }
+        // po eliminacji Gaussa, wyniki są zapisane w wektorze b
+        printToScreen(b);  // wypisanie rozwiązania z wektora b
     } else {
-        fprintf(stderr, "Błąd podczas eliminacji Gaussa!\n");
+        fprintf(stderr, "błąd podczas eliminacji gaussa!\n");
     }
 
     freeMatrix(A);
