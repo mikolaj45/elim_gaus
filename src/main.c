0a5c8b0712f3ecf79964358b6e1c2511c5316b21
Emilka <01197869@pw.edu.pl>
2024-12-20 23:48:46 +0100

    koncowe

--- src/main.c
+++ src/main.c
@@ -10,18 +10,23 @@ int main(int argc, char ** argv) {
 
     if (A == NULL) return -1;
     if (b == NULL) return -2;
+
+
     printToScreen(A);
     printToScreen(b);
 
-    if (A->r != b->r){
-        printf("złe rozmiary macierzy\n");
-        return -1;
+    //gdy niezgodne liczby wierszys
+    if (A->r != b->r) {
+        fprintf(stderr, "błąd: liczba wierszy macierzy A (%d) nie zgadza się z liczbą wierszy wektora b (%d).\n", A->r, b->r);
+        freeMatrix(A);
+        freeMatrix(b);
+        return 1;  
     }
     // wywołanie eliminacji Gaussa która obejmuje również podstawienie wsteczne
     res = eliminate(A, b);
 
     if (res == 0) {
-        // po eliminacji Gaussa, wyniki są zapisane w wektorze b
+        // po eliminacji Gaussa wyniki są zapisane w wektorze b
         printToScreen(b);  // wypisanie rozwiązania z wektora b
     } else {
         fprintf(stderr, "błąd podczas eliminacji gaussa!\n");
